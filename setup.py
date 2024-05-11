b04d3cc4aff45bff4bbd2eb5548274b247826a5b
David Cannan <104325852+Cdaprod@users.noreply.github.com>
2024-05-11 15:44:40 -0400

    Update push-to-pypi workflow configuration.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = os.getenv('PACKAGE_VERSION', '0.1.2') 
+version = os.getenv('PACKAGE_VERSION', '0.1.0') 
 
 setup(
     name='hydrate-minio-weaviate',
